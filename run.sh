3fb71b0f0761f0bb8ce533d79739141e54436942
Luis Capelo <luiscape@gmail.com>
2015-04-15 14:42:04 -0400

    fixing path

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 # Activating the DataStore
 source venv/bin/activate
-python code/create_datastore.py $HDX_KEY data/temp.csv
-python code/create_datastore_stag.py $HDX_KEY data/temp.csv
+python tool/code/create_datastore.py $HDX_KEY tool/data/temp.csv
+python tool/code/create_datastore_stag.py $HDX_KEY tool/data/temp.csv
